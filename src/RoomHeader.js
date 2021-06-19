8ab6d99d3200654efe3e1dd26ce199cd196221db
Shourya Gupta <shouryagupta@Shouryas-MacBook-Air.local>
2021-06-19 15:19:17 +0530

    add AddOthers funciton

--- src/RoomHeader.js
+++ src/RoomHeader.js
@@ -5,13 +5,18 @@ import VideocamOffIcon from '@material-ui/icons/VideocamOffTwoTone';
 import MicIcon from '@material-ui/icons/MicTwoTone';
 import MicOffIcon from '@material-ui/icons/MicOffTwoTone';
 import GroupAddIcon from '@material-ui/icons/GroupAddTwoTone';
-import DialogBox from './DialogBox';
+import Dialog from '@material-ui/core/Dialog'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Button from '@material-ui/core/Button';
 
-const RoomHeader = ({handleLogout, room}) => {
+const RoomHeader = ({handleLogout, room, roomName}) => {
 
     // state variables - used to swtich microphone and camera on/off
     const [isMic, setIsMic] = useState(true);
     const [isCamera, setIsCamera] = useState(true);
+
+    // state variable - to open/close dialog box
+    const [isOpen, setOpen] = useState(false); 
 
     // method to handle switch on/off
     const switchOnOff = (media) => (event) => {
@@ -43,31 +48,61 @@ const RoomHeader = ({handleLogout, room}) => {
         setIsCamera(!isCamera);
       }
     }
+
+    // methods - open / close the dialog box
+    const handleOpen = () => {
+      setOpen(true);
+    }
+    const handleClose = () =>{
+      setOpen(false);
+    }
+
+    // method - copy text (roomName) to clipboard and close the dialog box
+    const copyText = () => {
+      navigator.clipboard.writeText(roomName);
+      setOpen(false);
+    };
+
     return(
       <div id = "toolbar">
+
         {/* video camera icon */}
         <div>
           <button onClick = {switchOnOff("Cam")}>
           {isCamera?<VideocamIcon/>:<VideocamOffIcon/>}
           </button>
         </div>
+
         {/* microphone icon */}
         <div>
           <button onClick = {switchOnOff("Mic")}>
             {isMic?<MicIcon/>:<MicOffIcon/>}
           </button>
         </div>
+
         {/* End call icon */}
         <div>
           <button onClick = {handleLogout} style = {{backgroundColor:"#ff484e"}}>
             <CallEndIcon/>
           </button>
         </div>
+
         {/* Add others icon */}
         <div>
-          <button>
+
+          <button onClick = {handleOpen} style = {{background: "#00be5d"}}>
             <GroupAddIcon/>
           </button>
+
+          {/* Dialog box */}
+          <Dialog open = {isOpen} onClose = {handleClose} fullWidth = {true} maxWidth = {"sm"}>
+          <DialogTitle>Invite others to join you</DialogTitle>
+          <p style = {{textAlign : 'center'}}>Share the room name (<q>{roomName}</q>) with others who you wish to invite</p>
+            <Button onClick={copyText} autoFocus>
+			        Copy room name
+		        </Button>
+          </Dialog>
+          
         </div>
 
       </div>
